470ac8528a2dfa63e4fb948639cbf3e6ab286db0
Luis F Franco <luisferfranco@gmail.com>
2022-06-07 22:01:56 -0500

    Barras pre-update

--- 03-barra/js/app.js
+++ 03-barra/js/app.js
@@ -1,7 +1,7 @@
 const graf = d3.select("#graf")
 
 const margins = {
-  top: 20,
+  top: 50,
   right: 20,
   bottom: 70,
   left: 105,
@@ -18,36 +18,65 @@ const svg = graf
   .attr("height", altoTotal)
   .attr("class", "graf")
 
+svg
+  .append("rect")
+  .attr("x", 0)
+  .attr("y", 0)
+  .attr("width", ancho)
+  .attr("height", alto)
+  .attr("transform", `translate(${margins.left}, ${margins.top})`)
+  .classed("backdrop", true)
+
 const g = svg
   .append("g")
   .attr("transform", `translate(${margins.left}, ${margins.top})`)
 
-// g.append("rect")
-//   .attr("x", 0)
-//   .attr("y", 0)
-//   .attr("width", ancho)
-//   .attr("height", alto)
-
 const load = async () => {
-  let data = await d3.csv("barras.csv")
-  data.forEach((d) => {
-    d.clientes = +d.clientes
-    d.costos = +d.costos
-    d.facturacion = +d.facturacion
-    d.ganancia = +d.ganancia
-    d.margen = +d.margen
-  })
-  console.log(data)
+  let data = await d3.csv("barras.csv", d3.autoType)
+  data.sort((a, b) => b.clientes - a.clientes)
+
+  // Accessor
+  const xAccessor = (d) => d.tienda
+  const yAccessor = (d) => d.clientes
+
+  // Escaladores
+  const tiendas = d3.map(data, (d) => d.tienda)
+  const x = d3
+    .scaleBand()
+    .domain(tiendas)
+    .range([0, ancho])
+    .paddingOuter(0.2)
+    .paddingInner(0.1)
+  const y = d3
+    .scaleLinear()
+    .domain([0, d3.max(data, yAccessor)])
+    .range([alto, 0])
 
   const rect = g
     .selectAll("rect")
     .data(data)
     .enter()
     .append("rect")
-    .attr("x", (d, i) => i * 30)
-    .attr("y", 0)
-    .attr("width", 25)
-    .attr("height", (d) => d.ganancia)
+    .attr("x", (d) => x(xAccessor(d)))
+    .attr("y", (d) => y(yAccessor(d)))
+    .attr("width", x.bandwidth())
+    .attr("height", (d) => alto - y(yAccessor(d)))
+
+  g.append("text")
+    .attr("x", ancho / 2)
+    .attr("y", -10)
+    .attr("text-anchor", "middle")
+    .classed("titulo", true)
+    .text("Clientes por Tienda")
+
+  const xAxis = d3.axisBottom(x)
+  const xAxisGroup = g
+    .append("g")
+    .attr("transform", `translate(0, ${alto})`)
+    .classed("axis", true)
+    .call(xAxis)
+  const yAxis = d3.axisLeft(y).ticks(10)
+  const yAxisGroup = g.append("g").classed("axis", true).call(yAxis)
 }
 
 load()
