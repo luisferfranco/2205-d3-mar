67eee0d066e34b4c8d062bfbbaf5e7f93a96b715
Luis F Franco <luisferfranco@gmail.com>
2022-06-14 22:28:51 -0500

    Barras completas

--- 03-barra/js/app.js
+++ 03-barra/js/app.js
@@ -37,7 +37,6 @@ const g = svg
 
 const draw = async (m = "clientes") => {
   let data = await d3.csv("barras.csv", d3.autoType)
-  data.sort((a, b) => b.clientes - a.clientes)
 
   // Llenar el select
   // console.log(data)
@@ -52,59 +51,79 @@ const draw = async (m = "clientes") => {
 
   // Accessor
   const xAccessor = (d) => d.tienda
-  const yAccessor = (d) => d[m]
 
   // Escaladores
-  const tiendas = d3.map(data, (d) => d.tienda)
-  const x = d3
-    .scaleBand()
-    .domain(tiendas)
-    .range([0, ancho])
-    .paddingOuter(0.2)
-    .paddingInner(0.1)
-  const y = d3
-    .scaleLinear()
-    .domain([0, d3.max(data, yAccessor)])
-    .range([alto, 0])
+  const x = d3.scaleBand().range([0, ancho]).paddingOuter(0.2).paddingInner(0.1)
+  const y = d3.scaleLinear().range([alto, 0])
+  const color = d3
+    .scaleOrdinal()
+    .domain(Object.keys(data[0]).slice(1))
+    .range(d3.schemePastel1)
 
-  // Dibujo de las barras
-  const rect = g
-    .selectAll("rect")
-    .data(data)
-    .enter()
-    .append("rect")
-    .attr("x", (d) => x(xAccessor(d)))
-    .attr("y", (d) => y(yAccessor(d)))
-    .attr("width", x.bandwidth())
-    .attr("height", (d) => alto - y(yAccessor(d)))
-
-  // Título
-  g.append("text")
+  // Titulo
+  const titulo = g
+    .append("text")
     .attr("x", ancho / 2)
     .attr("y", -10)
     .attr("text-anchor", "middle")
     .classed("titulo", true)
-    .text("Clientes por Tienda")
 
   // Ejes
-  const xAxis = d3.axisBottom(x)
   const xAxisGroup = g
     .append("g")
     .attr("transform", `translate(0, ${alto})`)
     .classed("axis", true)
-    .call(xAxis)
-  const yAxis = d3.axisLeft(y).ticks(10)
-  const yAxisGroup = g.append("g").classed("axis", true).call(yAxis)
+  const yAxisGroup = g.append("g").classed("axis", true)
+
+  const render = (m) => {
+    // Accesores
+    const yAccessor = (d) => d[m]
+
+    // Ordenamiento de datos
+    data.sort((a, b) => b[m] - a[m])
+
+    // Escaladores
+    const tiendas = d3.map(data, (d) => d.tienda)
+    x.domain(tiendas)
+    y.domain([0, d3.max(data, yAccessor)])
+
+    // Dibujo de las barras
+    const rect = g.selectAll("rect").data(data)
+    rect
+      .enter()
+      .append("rect")
+      .attr("x", (d) => x(xAccessor(d)))
+      .attr("y", y(0))
+      .attr("width", x.bandwidth())
+      .attr("height", 0)
+      .attr("fill", "green")
+      .merge(rect)
+      .transition()
+      .duration(2000)
+      .attr("x", (d) => x(xAccessor(d)))
+      .attr("y", (d) => y(yAccessor(d)))
+      .attr("width", x.bandwidth())
+      .attr("height", (d) => alto - y(yAccessor(d)))
+      .attr("fill", color(m))
+
+    // Título
+    titulo.text(m + " por Tienda")
+
+    // Ejes
+    const xAxis = d3.axisBottom(x)
+    const yAxis = d3.axisLeft(y).ticks(10)
+    xAxisGroup.transition().duration(2000).call(xAxis)
+    yAxisGroup.transition().duration(2000).call(yAxis)
+  }
 
   // Escucha de Eventos
   metrica.on("change", (e) => {
     e.preventDefault()
     console.log(metrica.node().value)
+    render(metrica.node().value)
   })
 
-  d3.select("#formu").on("submit", (e) => {
-    e.preventDefault()
-  })
+  render(m)
 }
 
 draw()
