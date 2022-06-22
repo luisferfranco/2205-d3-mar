49f885aed0988b1a89d6d4cf2bb4945d4a3595a7
Luis F Franco <luisferfranco@gmail.com>
2022-06-21 20:59:57 -0500

    Heatmap Inicial

---
+++ 04-heatmap/js/app.js
@@ -0,0 +1,20 @@
+const graf = d3.select("#graf")
+
+const anchoTotal = +graf.style("width").slice(0, -2)
+const altoTotal = anchoTotal * 0.5
+
+const margen = { arriba: 50, derecha: 10, abajo: 95, izquierda: 90 }
+
+const ancho = anchoTotal - margen.izquierda - margen.derecha
+const alto = altoTotal - margen.arriba - margen.abajo
+
+const svg = graf
+  .append("svg")
+  .attr("width", anchoTotal)
+  .attr("height", altoTotal)
+
+const draw = (el) => {
+  const dataset = await d3.csv("data.csv", d3.autoType)
+}
+
+draw("#hm1")
