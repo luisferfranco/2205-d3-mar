ea39fb7bb136341bbcc62d1151952e4071cd7d7a
Luis F Franco <luisferfranco@gmail.com>
2022-06-28 23:08:17 -0500

    Gapminder Final

--- 06-gapminder-0/js/app.js
+++ 06-gapminder-0/js/app.js
@@ -3,8 +3,9 @@ const draw = async (el = "#graf") => {
   const graf = d3.select("#graf")
 
   // Carga del dataset
-  const dataset = await d3.csv("gapminder.csv", d3.autoType)
-  console.log(dataset)
+  let dataset = await d3.csv("gapminder.csv", d3.autoType)
+  dataset = dataset.filter((d) => d.life_exp > 0 && d.income > 0)
+  // console.log(dataset)
 
   // Dimensiones
   const anchoTotal = +graf.style("width").slice(0, -2)
@@ -20,9 +21,19 @@ const draw = async (el = "#graf") => {
   const yAccessor = (d) => d.life_exp
   const rAccessor = (d) => d.population
 
+  // Variables
+  var year = d3.max(dataset, (d) => d.year)
+  console.log(year)
+
   // Escaladores
+  console.log(Array.from(new Set(dataset.map((d) => d.continent))))
+  const color = d3
+    .scaleOrdinal()
+    .domain(Array.from(new Set(dataset.map((d) => d.continent))))
+    .range(["#ff798e", "#33dded", "#99ef33", "#ffec33"])
+
   const x = d3
-    .scaleLinear()
+    .scaleLog()
     .domain(d3.extent(dataset, xAccessor))
     .range([0, ancho])
   const y = d3
@@ -33,7 +44,7 @@ const draw = async (el = "#graf") => {
   const r = d3
     .scaleLinear()
     .domain(d3.extent(dataset, rAccessor))
-    .range([2, 150])
+    .range([30, 6000])
     .nice()
 
   // Espacio de gráfica
@@ -42,6 +53,13 @@ const draw = async (el = "#graf") => {
     .classed("graf", true)
     .attr("width", anchoTotal)
     .attr("height", altoTotal)
+
+  const clip = svg
+    .append("clipPath")
+    .attr("id", "clip")
+    .append("rect")
+    .attr("width", ancho)
+    .attr("height", alto)
 
   svg
     .append("g")
@@ -55,20 +73,65 @@ const draw = async (el = "#graf") => {
     .append("g")
     .attr("transform", `translate(${margins.left}, ${margins.top})`)
 
-  // Dibujar los puntos
-  const circles = chart
-    .selectAll("circle")
-    .data(dataset)
-    .join("circle")
-    .attr("cx", (d) => x(xAccessor(d)))
-    .attr("cy", (d) => y(yAccessor(d)))
-    .attr("r", (d) => r(rAccessor(d)))
+  const yearLayer = chart
+    .append("g")
+    .append("text")
+    .attr("x", ancho / 2)
+    .attr("y", alto / 2)
+    .attr("text-anchor", "middle")
+    .attr("alignment-baseline", "central")
+    .classed("year", true)
+    .text(year)
+
+  let newDataset
+
+  const step = (year) => {
+    newDataset = dataset.filter((d) => d.year == year)
+
+    // Dibujar los puntos
+    const circles = chart
+      .selectAll("circle")
+      .data(newDataset)
+      .join(
+        (enter) =>
+          enter
+            .append("circle")
+            .attr("cx", (d) => x(xAccessor(d)))
+            .attr("cy", (d) => y(yAccessor(d)))
+            .attr("r", 0)
+            .attr("fill", "black")
+            .on("mouseenter", function (event, datum) {
+              d3.select("#country").text(datum.country)
+              d3.select("#country-income").text(datum.income)
+              d3.select("#country-lifeexp").text(datum.life_exp)
+              d3.select("#country-population").text(datum.population)
+              d3.select("#tooltip")
+                .style("display", "block")
+                .style("top", y(yAccessor(datum)) + margins.top + "px")
+                .style("left", x(xAccessor(datum)) + margins.left + "px")
+            })
+            .on("mouseout", function (event, datum) {
+              d3.select("#tooltip").style("display", "none")
+            }),
+
+        (update) => update,
+        (exit) =>
+          exit.transition().duration(2000).attr("r", 0).attr("fill", "red")
+      )
+      .transition()
+      .duration(2000)
+      .attr("r", (d) => Math.sqrt(r(rAccessor(d)) / Math.PI))
+      .attr("fill", (d) => color(d.continent))
+      .attr("stroke", "#555")
+      .attr("opacity", 0.6)
+      .attr("clip-path", "url(#clip)")
+  }
 
   // Ejes
   const xAxis = d3
     .axisBottom(x)
-    .ticks(5)
-    .tickFormat((d) => d.toLocaleString())
+    .ticks(3)
+    .tickFormat((d) => "$" + d.toLocaleString())
   const yAxis = d3.axisLeft(y)
 
   const xAxisGroup = chart
@@ -92,6 +155,19 @@ const draw = async (el = "#graf") => {
     .style("text-anchor", "middle")
     .style("transform", "rotate(270deg)")
     .text("Expectativa de Vida")
+
+  d3.select("#atras").on("click", () => {
+    year--
+    step(year)
+    yearLayer.text(year)
+  })
+  d3.select("#adelante").on("click", () => {
+    year++
+    step(year)
+    yearLayer.text(year)
+  })
+
+  step(year)
 }
 
 draw()
